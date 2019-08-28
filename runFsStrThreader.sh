05ae666164038a3c56d05e2e15346e0665c0562f
hernanmd <hernan.morales@gmail.com>
2019-08-28 17:51:40 -0300

    Add script to create directory with family names

--- runFsStrThreader.sh
+++ runFsStrThreader.sh
@@ -1,19 +1,23 @@
-#!/bin/bash 
+#!/bin/bash
 
-# Paper: https://onlinelibrary.wiley.com/doi/full/10.1111/1755-0998.12702
-# Manual: https://github.com/StuntsPT/Structure_threader/blob/master/docs/usage.md
+# Paper: Structure_threader https://onlinelibrary.wiley.com/doi/full/10.1111/1755-0998.12702
+# Manual 1 : https://github.com/StuntsPT/Structure_threader/blob/master/docs/usage.md
+# Manual 2 : https://structure-threader.readthedocs.io/en/latest/
+# Sample run
+#
+# ./runFsStrThreader fs_input_dir/ fs_output_dir/
 
-project_dir=""
-project_output=""
+project_dir="$1"
+project_output="$2"
 fsbin="/data/home/hernan/.local/bin/fastStructure"
 strbin="/usr/local/bin/structure"
 
 # Generate mainparams and extraparams
-structure_threader params -o "$project_output" \
+structure_threader params -o . \
 
 # With fastSTRUCTURE
 structure_threader run \
-	-K 4 9 \
+	-K 9 \
 	-R 1 \
 	-i "$project_dir"/project_data \
 	-o "$project_output" \
@@ -21,7 +25,7 @@ structure_threader run \
 	-st "$strbin" \
 	--params mainparams \
 	--log 1
-	
+
 structure_threader plot \
 	-i fS_run \
 	-f fastStructure \
