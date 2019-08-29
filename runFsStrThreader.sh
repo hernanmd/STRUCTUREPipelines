be20d8dfd9dabb7f270d375a6d0454cc85ab3f23
hernanmd <hernan.morales@gmail.com>
2019-08-29 14:44:35 -0300

    Split fastSTRCUTURE and STRUCTURE analysis script

--- runFsStrThreader.sh
+++ runFsStrThreader.sh
@@ -7,28 +7,28 @@
 #
 # ./runFsStrThreader fs_input_dir/ fs_output_dir/
 
-project_dir="$1"
+project_input="$1"
 project_output="$2"
+pop_file="$3"
+ks="$4"
 fsbin="/data/home/hernan/.local/bin/fastStructure"
 strbin="/usr/local/bin/structure"
 
 # Generate mainparams and extraparams
 structure_threader params -o . \
 
+echo "Please edit the generated mainparams file..."
+
 # With fastSTRUCTURE
 structure_threader run \
-	-K 9 \
+	-K "$ks" \
 	-R 1 \
-	-i "$project_dir"/project_data \
+	-i "$project_input" \
 	-o "$project_output" \
 	-t 32 \
-	-st "$strbin" \
+	-fs "$fsbin" \
+	--pop "$pop_file" \
 	--params mainparams \
 	--log 1
 
-structure_threader plot \
-	-i fS_run \
-	-f fastStructure \
-	-K 2 3 4 \
-	-o 2_4_plots \
-	--ind indfile.txt
+runPlotStrThreader "project_output" "$pop_file"
